193a25af4a1084f41767f20d4414fdabc1a4680b
Sai Nandini Devabattula <sainandinidevabattula97@gmail.com>
2023-04-11 20:24:12 -0500

    updated

--- controllers/soaps.js
+++ controllers/soaps.js
@@ -1,8 +1,17 @@
 var soaps = require('../models/soaps');
 // List of all Costumes
-exports.soaps_list = function(req, res) {
- res.send('NOT IMPLEMENTED: soaps list');
-};
+exports.soaps_list = async function(req, res) {
+
+ try{
+    thesoaps = await soaps.find();
+    res.send(thesoaps);
+    }
+    catch(err){
+    res.status(500);
+    res.send(`{"error": ${err}}`);
+    }
+   };
+
 // for a specific Costume.
 exports.soaps_detail = function(req, res) {
  res.send('NOT IMPLEMENTED: soaps detail: ' + req.params.id);
@@ -19,3 +28,15 @@ exports.soaps_delete = function(req, res) {
 exports.soaps_update_put = function(req, res) {
  res.send('NOT IMPLEMENTED: soaps update PUT' + req.params.id);
 };
+// VIEWS
+// Handle a show all view
+exports.soaps_view_all_Page = async function(req, res) {
+    try{
+    thesoaps = await soaps.find();
+    res.render('soaps', { title: 'soaps Search Results', results: thesoaps });
+    }
+    catch(err){
+    res.status(500);
+    res.send(`{"error": ${err}}`);
+    }
+   };
--- routes/soaps.js
+++ routes/soaps.js
@@ -1,9 +1,9 @@
 var express = require('express');
+const soaps_controllers= require('../controllers/soaps');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('soaps', { title: 'Search Results soap' });
-});
-
+/* GET costumes */
+router.get('/', soaps_controllers.soaps_view_all_Page );
 module.exports = router;
+
