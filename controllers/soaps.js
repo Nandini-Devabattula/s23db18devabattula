f45fcdd2da8daa0925ca1e1fde9d3edff09a869d
Sai Nandini Devabattula <sainandinidevabattula97@gmail.com>
2023-04-18 19:41:06 -0500

    part 8

--- controllers/soaps.js
+++ controllers/soaps.js
@@ -95,7 +95,63 @@ exports.soaps_view_all_Page = async function(req, res) {
     res.send(`{"error": ${err}}`);
     }
    };
+
+   // Handle a show one view with id specified by query
+exports.soaps_view_one_Page = async function(req, res) {
+    console.log("single view for id " + req.query.id)
+    try{
+    result = await soaps.findById( req.query.id)
+    res.render('soapsdetail',
+   { title: 'Soaps Detail', toShow: result });
+    }
+    catch(err){
+    res.status(500)
+    res.send(`{'error': '${err}'}`);
+    }
+   };
   
+   // Handle building the view for creating a costume.
+// No body, no in path parameter, no query.
+// Does not need to be async
+exports.soaps_create_Page = function(req, res) {
+    console.log("create view")
+    try{
+    res.render('soapscreate', { title: 'Soaps Create'});
+    }
+    catch(err){
+    res.status(500)
+    res.send(`{'error': '${err}'}`);
+    }
+    };
+
+    // Handle building the view for updating a costume.
+// query provides the id
+exports.soaps_update_Page = async function(req, res) {
+    console.log("update view for item "+req.query.id)
+    try{
+    let result = await soaps.findById(req.query.id)
+    res.render('soapsupdate', { title: 'Soaps Update', toShow: result });
+    }
+    catch(err){
+    res.status(500)
+    res.send(`{'error': '${err}'}`);
+    }
+    };
+    
+exports.soaps_delete_Page = async function(req, res) {
+    console.log("Delete view for id " + req.query.id)
+    try{
+    result = await soaps.findById(req.query.id)
+    res.render('soapsdelete', { title: 'Soaps Delete', toShow:
+    result });
+    }
+    catch(err){
+    res.status(500)
+    res.send(`{'error': '${err}'}`);
+    }
+    };
+    
+
    // for a specific soaps.
     exports.soaps_detail = async function(req, res) {
     console.log("detail" + req.params.id)
--- routes/soaps.js
+++ routes/soaps.js
@@ -6,5 +6,11 @@ var router = express.Router();
 /* GET soapss */
 //GET request for one soaps.
 router.get('/soaps/:id', soaps_controllers.soaps_detail);
+router.get('/detail', soaps_controllers.soaps_view_one_Page);
+router.get('/create', soaps_controllers.soaps_create_Page);
+router.get('/update', soaps_controllers.soaps_update_Page);
+router.get('/delete', soaps_controllers.soaps_delete_Page);
+
+
 module.exports = router;
 
