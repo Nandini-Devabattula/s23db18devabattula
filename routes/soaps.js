4bf9307110619432f46b2e87091e544d77236081
Sai Nandini Devabattula <sainandinidevabattula97@gmail.com>
2023-04-21 12:30:49 -0500

    updated

--- routes/soaps.js
+++ routes/soaps.js
@@ -5,7 +5,7 @@ var router = express.Router();
 /* GET home page. */
 /* GET soapss */
 //GET request for one soaps.
-router.get('/soaps/:id', soaps_controllers.soaps_detail);
+router.get('/', soaps_controllers.soaps_view_all_Page);
 router.get('/detail', soaps_controllers.soaps_view_one_Page);
 router.get('/create', soaps_controllers.soaps_create_Page);
 router.get('/update', soaps_controllers.soaps_update_Page);
