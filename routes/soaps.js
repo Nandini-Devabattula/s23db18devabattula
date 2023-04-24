eea886a01d5e2a61ba748e6f99bd0d9c2c29ffda
Sai Nandini Devabattula <sainandinidevabattula97@gmail.com>
2023-04-24 18:13:41 -0500

    Updated part 4

--- routes/soaps.js
+++ routes/soaps.js
@@ -2,6 +2,7 @@ var express = require('express');
 const soaps_controllers= require('../controllers/soaps');
 var router = express.Router();
 
+
 /* GET home page. */
 /* GET soapss */
 //GET request for one soaps.
